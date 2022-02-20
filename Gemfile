1e967a6cf3791da8bc58311a9381e1f9ddc2575b
Ray Bradley <ray.bradley@gmail.com>
2022-02-19 20:55:14 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "jekyll", "~> 4.2.1"
+gem "jekyll-postcss"
+gem "minima", "~> 2.5"
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+// https://stefcoetzee.com/2021/07/28/jekyll-tailwind-starter-repo
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content:[
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './**/*.html',
+    './*.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        brand: {
+          100: '#DEE3E8',
+          200: '#C9D8E8',
+          300: '#5F9BDA',
+          400: '#2E7FD3',
+          500: '#E66425',
+          600: '#004A91',
+          700: '#002C56',
+          800: '#11273B',
+          900: '#121920',     
+        }
+      }      
+    },
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
